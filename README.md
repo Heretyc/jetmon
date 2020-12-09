163ea7e2e8c2a6656cfe15e7a68c0bed24fc5814
Brandon Blackburn <contact@bhax.net>
2020-12-09 16:00:19 -0600

    Added: Compilation support and instructions

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # jetmon
 Verizon MiFi Jetpack monitoring
 
+## Standalone builds
+
+For a standalone binary run the following command inside the Pipenv:
+
+`pyinstaller --icon=blackburnDevIcon.ico -F monitor.py`
+
 ## Installation
 
 To drive Firefox, you have to download geckodriver and put it in the same folder as the Python executables.
--- monitor.py
+++ monitor.py
@@ -3,47 +3,71 @@ from selenium.webdriver.firefox.options import Options
 import pathlib
 import os
 from win10toast import ToastNotifier
+import time
 
-# create an object to ToastNotifier class
-notifier_object = ToastNotifier()
+if __name__ == "__main__":
+    print("Monitor started...")
+    while True:
 
-
-url = 'http://my.jetpack/'
-page_title = 'Jetpack MiFi 8800L'
-
-driver_location = pathlib.Path.cwd() / "geckodriver"
-icon_location = pathlib.Path.cwd() / "blackburnDevIcon.ico"
-
-options = Options()
-# We use all methods available to ensure the browser is headless
-# Depending on OS, Firefox build and execution environment, some work, others do not
-options.headless = True
-options.add_argument("-headless")
-os.environ['MOZ_HEADLESS'] = '1'
-driver = webdriver.Firefox(options=options, executable_path=driver_location)
-
-browser = webdriver.Firefox(executable_path=driver_location)
-browser.implicitly_wait(3)
+        # create an object to ToastNotifier class
+        notifier_object = ToastNotifier()
 
 
-browser.get(url)
-title = browser.title
-if page_title not in title:
-    exit(1)
-status_battery = browser.find_element_by_css_selector('#statusBar_battery').text
-status_rssi = browser.find_element_by_css_selector('#statusBar_rssi').get_attribute('class')
-status_tech = browser.find_element_by_css_selector('#statusBar_tech').get_attribute('class')
-browser.quit()
+        url = 'http://my.jetpack/'
+        page_title = 'Jetpack MiFi 8800L'
+        interval_poll = 15
+        interval_sleep = 120
 
-status_battery = int(status_battery.replace('%', ''))
-status_rssi = int(status_rssi.replace('rssi_', ''))
-status_tech = status_tech.replace('tech_', '')
+        driver_location = pathlib.Path.cwd() / "geckodriver"
+        icon_location = pathlib.Path.cwd() / "blackburnDevIcon.ico"
 
-percent_rssi = int((status_rssi/5)*100)
+        options = Options()
+        # We use all methods available to ensure the browser is headless
+        # Depending on OS, Firefox build and execution environment, some work, others do not
+        options.headless = True
+        options.add_argument("-headless")
+        os.environ['MOZ_HEADLESS'] = '1'
+        driver = webdriver.Firefox(options=options, executable_path=driver_location)
 
-notifier_object.show_toast("Jetpack Status", f"""Battery:{status_battery}%
-Signal: {percent_rssi}%
-Network: {status_tech}""", duration=30, icon_path=icon_location)
+        browser = webdriver.Firefox(executable_path=driver_location)
+        browser.implicitly_wait(3)
+
+
+        browser.get(url)
+        title = browser.title
+        if page_title not in title:
+            notifier_object.show_toast("Jetpack Status", f"Jetpack disconnected", duration=interval_poll, icon_path=icon_location)
+        status_battery = browser.find_element_by_css_selector('#statusBar_battery').text
+        status_rssi = browser.find_element_by_css_selector('#statusBar_rssi').get_attribute('class')
+        status_tech = browser.find_element_by_css_selector('#statusBar_tech').get_attribute('class')
+
+
+        attempts_left = 10
+        while attempts_left > 0:
+            attempts_left -= 1
+            try:
+                status_battery = int(status_battery.replace('%', ''))
+                break
+            except ValueError:
+                status_battery = browser.find_element_by_css_selector('#statusBar_battery').text
+
+
+        status_rssi = int(status_rssi.replace('rssi_', ''))
+        status_tech = status_tech.replace('tech_', '')
+
+        browser.quit()
+
+        percent_rssi = int((status_rssi/5)*100)
+
+        if percent_rssi < 30:
+            notifier_object.show_toast("Jetpack Status", f"Poor signal using {status_tech}", duration=interval_poll, icon_path=icon_location)
+        elif status_battery < 10:
+            notifier_object.show_toast("Jetpack Status", f"Battery at {status_battery}%!", duration=interval_poll, icon_path=icon_location)
+        else:
+            time.sleep(interval_sleep)
+#             notifier_object.show_toast("Jetpack Status", f"""Battery:{status_battery}%
+# Signal: {percent_rssi}%
+# Network: {status_tech}""", duration=interval_sleep, icon_path=icon_location)
 
 
 
