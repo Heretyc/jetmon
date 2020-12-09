60a6a190d8e2ae85559c140463df881c03cea7f6
Brandon Blackburn <contact@bhax.net>
2020-12-09 14:53:54 -0600

    Added: Windows 10 notifications

--- monitor.py
+++ monitor.py
@@ -2,11 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import pathlib
 import os
+from win10toast import ToastNotifier
+
+# create an object to ToastNotifier class
+notifier_object = ToastNotifier()
+
 
 url = 'http://my.jetpack/'
 page_title = 'Jetpack MiFi 8800L'
 
 driver_location = pathlib.Path.cwd() / "geckodriver"
+icon_location = pathlib.Path.cwd() / "blackburnDevIcon.ico"
 
 options = Options()
 # We use all methods available to ensure the browser is headless
@@ -27,9 +33,18 @@ if page_title not in title:
 status_battery = browser.find_element_by_css_selector('#statusBar_battery').text
 status_rssi = browser.find_element_by_css_selector('#statusBar_rssi').get_attribute('class')
 status_tech = browser.find_element_by_css_selector('#statusBar_tech').get_attribute('class')
-
-status_battery = status_battery.replace('%', '')
-status_rssi = status_rssi.replace('rssi_', '')
-status_tech = status_tech.replace('tech_', '')
 browser.quit()
+
+status_battery = int(status_battery.replace('%', ''))
+status_rssi = int(status_rssi.replace('rssi_', ''))
+status_tech = status_tech.replace('tech_', '')
+
+percent_rssi = int((status_rssi/5)*100)
+
+notifier_object.show_toast("Jetpack Status", f"""Battery:{status_battery}%
+Signal: {percent_rssi}%
+Network: {status_tech}""", duration=30, icon_path=icon_location)
+
+
+
 print("Finished.")
